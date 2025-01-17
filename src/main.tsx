26f3af6920e5a3e37ea3d280cb3f0ba14bab0363
nevzattalhaozcan <ozcann.talha@gmail.com>
2025-01-17 04:54:09 +0300

    update: add basename ot main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,46 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
-import Posts from './pages/Posts'
-import Profile from './pages/Profile'
-import App from './App'
-import { ThemeProvider } from './contexts/ThemeContext'
-import { AuthProvider } from './contexts/AuthContext'
-import ProtectedRoute from './components/ProtectedRoute'
-import Error from './pages/Error'
-import PostDetail from './components/PostDetail'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import Profile from './pages/Profile';
+import App from './App';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Error from './pages/Error';
+import PostDetail from './components/PostDetail';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: '/blog-app',
+          element: <Home />,
+        },
+        {
+          path: '/posts',
+          element: <Posts />,
+        },
+        {
+          path: '/posts/:id',
+          element: <PostDetail />,
+        },
+        {
+          path: '/profile',
+          element: (
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          ),
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/blog-app",
-        element: <Home />
-      },
-      {
-        path: "/posts",
-        element: <Posts />
-      },
-      {
-        path: "/posts/:id",
-        element: <PostDetail />
-      },
-      {
-        path: "/profile",
-        element: (
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        )
-      }
-    ]
+    basename: '/blog-app',
   }
-]);
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -49,5 +54,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <RouterProvider router={router} />
       </AuthProvider>
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
